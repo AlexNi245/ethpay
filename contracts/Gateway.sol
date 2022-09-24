18fe7137feb27997829c54ba399cded3e57ec512
Alex <alex.plutta@googlemail.com>
2022-09-24 21:59:00 +0200

    Init gateway with any token

---
+++ contracts/Gateway.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IGateway.sol";
+
+contract Gateway is IGateway {
+    address public immutable token;
+
+    constructor(address _token) {
+        token = _token;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public override returns (bool) {
+        return true;
+    }
+}
---
+++ contracts/IGateway.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.2;
+
+interface IGateway {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
