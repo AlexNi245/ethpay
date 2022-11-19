45b57d50a4f2596d090188ed7434b3cf6683960d
Alex <alex.plutta@googlemail.com>
2022-11-19 22:48:40 +0100

    add index.ts

--- ethpay/offchain-processor/index.ts
+++ ethpay/offchain-processor/index.ts
@@ -1,15 +1,52 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { loginResource } from "./resource/loginResource";
+import { PrismaClient } from "@prisma/client";
+import { paymentResource } from "./resource/paymentResource";
+import { tokenResource } from "./resource/tokenResource";
+import { ethers } from "hardhat";
+import { GatewayService } from "./service/GatewayService";
+import { Wallet } from "ethers";
+import { GatewayRegistry } from "../typechain";
 
 dotenv.config();
+export const main = async () => {
 
-const app: Express = express();
-const port = 3010;
+    const app: Express = express();
+    const port = 3010;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+    const db = new PrismaClient();
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+    const { REGISTRY_ADDRESS, ONCHAIN_PROCESSOR_PRIVATE_KEY } = process.env;
+
+    if (!REGISTRY_ADDRESS) {
+        throw "Undefined registry";
+    }
+
+    if (!ONCHAIN_PROCESSOR_PRIVATE_KEY) {
+        throw "Undefined onchain processor";
+    }
+
+    const gatewayRegistry = (
+        await ethers.getContractFactory("GatewayRegistry")
+    ).attach(REGISTRY_ADDRESS) as GatewayRegistry;
+
+    const onchainProcessor = new Wallet(ONCHAIN_PROCESSOR_PRIVATE_KEY);
+
+    app.use("/login", loginResource(db));
+    app.use(
+        "/payment",
+        paymentResource(
+            db,
+            new GatewayService(gatewayRegistry, onchainProcessor)
+        )
+    );
+    app.use("/token", tokenResource(db, gatewayRegistry, onchainProcessor));
+
+    app.listen(port, () => {
+        console.log(
+            `⚡️[server]: Server is running at https://localhost:${port}`
+        );
+    });
+};
+main();
