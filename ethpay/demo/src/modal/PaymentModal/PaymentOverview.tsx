b70d333fdfd45e315e3008521b528404cdc4781e
Alex <alex.plutta@googlemail.com>
2022-11-24 20:05:53 +0100

    add payment modal

--- ethpay/demo/src/modal/PaymentModal/PaymentOverview.tsx
+++ ethpay/demo/src/modal/PaymentModal/PaymentOverview.tsx
@@ -1,16 +1,40 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { useContext } from "react";
+import { useAccount } from "wagmi";
 import { ModalContext } from "../../context/ModalContext";
+import { PaymentContext } from "../../context/PaymentContext";
 import { PaymentModalContext, STEPS } from "./context/PaymentModalContext";
 
 export const PaymentOverview = () => {
     const { payment, setSteps } = useContext(PaymentModalContext);
     const { onClose } = useContext(ModalContext);
-    const onContinue = () => {
+    const { makeBtcPayment, makeMaticPayment, makeUsdcPayment } =
+        useContext(PaymentContext);
+    const { address } = useAccount();
+    const { name, receiver, amount, currency, token } = payment as any;
+
+    const onContinue = async () => {
         setSteps(STEPS.Confirmation);
+        //WMATIC
+        if (token === "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270") {
+            console.log("Start wMatic payment");
+            await makeMaticPayment(address as string, amount, receiver);
+            return onClose();
+        }
+        //USDC
+        if (token === "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174") {
+            console.log("Start usdc payment");
+            await makeUsdcPayment(address as string, amount, receiver);
+            return onClose();
+        }
+        //WBTC
+        if (token === "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6") {
+            console.log("Start wbtc payment");
+            await makeBtcPayment(address as string, amount, receiver);
+            return onClose();
+        }
     };
 
-    const { name, receiver, amount, currency } = payment as any;
     return (
         <Flex flexDirection="column">
             <Text fontWeight="bold">
--- ethpay/demo/src/storage/Database.ts
+++ ethpay/demo/src/storage/Database.ts
@@ -1,5 +1,11 @@
+export interface PaymentDto {
+    name: string;
+    id: string;
+    createdAt: Date;
+}
 export class Database {
     private readonly sessionKey = "ETHPAY_AUTH_STORAGE";
+    private readonly itemsKey = "ETHPAY_ITEMS";
 
     public login(address: string, token: string) {
         const oldItems = JSON.parse(
@@ -25,5 +31,44 @@ export class Database {
         }
 
         return session.jwt;
+    }
+
+    public addItem(address: string, name: string, id: string) {
+        const oldUsers = JSON.parse(
+            localStorage.getItem(this.itemsKey) ?? "{}"
+        );
+
+        const oldItems = oldUsers[address] ?? [];
+
+        const newItems = [
+            ...oldItems,
+            {
+                name,
+                id,
+                createdAt: new Date(),
+            } as PaymentDto,
+        ];
+
+        const newUsers = {
+            ...oldUsers,
+            [address]: newItems,
+        };
+        console.log(newUsers);
+        localStorage.setItem(this.itemsKey, JSON.stringify(newUsers));
+    }
+
+    public getItems() {
+        const itemsString = localStorage.getItem(this.itemsKey);
+        const users = JSON.parse(itemsString ?? "{}");
+
+        const keys = Object.keys(users);
+
+        const items = keys.reduce(
+            (cur, agg) => [...cur, ...users[agg]],
+            [] as any[]
+        );
+
+
+        return items;
     }
 }
